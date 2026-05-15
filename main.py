0a913c25ee9b9ff56f1640e8bb2feeeae7e59f1f
RodoYd <arenaapptv@gmail.com>
2026-05-15 14:27:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ import uvicorn
 # ═══════════════════════════════════════════
 # الإعدادات (غيّرها حسب احتياجك)
 # ═══════════════════════════════════════════
-BOT_TOKEN = "PUT_YOUR_BOT_TOKEN_HERE"
-ADMIN_ID = 123456789                     # ID حسابك في تلجرام
+BOT_TOKEN = "8260979666:AAHkg21xZmD5svkyswqu9ascEz1pJf2P0Kg"
+ADMIN_ID = 8266981888                    # ID حسابك في تلجرام
 API_HOST = "127.0.0.1"
 API_PORT = 8000
 API_KEY = "CHANGE_THIS_SECRET_KEY"       # مفتاح لحماية API
